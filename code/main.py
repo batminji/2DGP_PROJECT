a59628e586bf72e14d610678fcf10ce7d2fcf490
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-10-16 19:59:15 +0900

    main.py 파일에 reset_world, update_world, render_world 구현. game loop 구현. title.py 추가.

---
+++ code/main.py
@@ -0,0 +1,48 @@
+from pico2d import *
+
+from title import*
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+def reset_world():
+    global running
+    global world
+
+    running = True
+    world = []
+
+    # grass = Grass()
+    # world.append(grass)
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+
+close_canvas()
