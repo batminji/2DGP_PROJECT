808634ead5c379d1bccfe3accbf7a32844bb8b4b
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-10-16 20:48:21 +0900

    로고 움직임 구현. 타이틀 화면 캐릭터 이동 구현. 캐릭터 속도 및 위치, 크기 조정

--- code/main.py
+++ code/main.py
@@ -2,7 +2,8 @@ from pico2d import *
 
 from title import Title
 
-SCREENX , SCREENY = 1915, 1015
+SCREENX, SCREENY = 1915, 1015
+
 
 def handle_events():
     global running
@@ -19,11 +20,17 @@ def reset_world():
     global running
     global world
 
+    global title
+
     running = True
     world = []
 
     # grass = Grass()
     # world.append(grass)
+
+    title = Title()
+    world.append(title)
+
 
 def update_world():
     for o in world:
@@ -45,6 +52,6 @@ while running:
     handle_events()
     update_world()
     render_world()
-    delay(0.01)
+    delay(0.05)
 
 close_canvas()
--- code/title.py
+++ code/title.py
@@ -1,27 +1,36 @@
-from pico2d import*
+from pico2d import *
 import random
 
-from main import SCREENX, SCREENY
+SCREENX, SCREENY = 1915, 1015
+
 
 class Title:
     def __init__(self):
         self.background = load_image('title_background.png')
         self.x, self.y = SCREENX // 2, SCREENY // 2
         self.logo = load_image('title_logo.png')
-        self.logo_x = 1200
+        self.logo_x = 2500
         self.char1 = load_image('player_run.png')
         self.char2 = load_image('ai_run.png')
-        self.frame1, self.frame2 = random.randint(0, 6), random.randint(0,6)
+        self.frame1, self.frame2 = random.randint(0, 6), random.randint(0, 6)
         self.char1_x, self.char2_x = 0, 0
-        self.char1_speed, self.char2_speed = 10, 20
+        self.char1_speed, self.char2_speed = 10, 15
+
     def update(self):
         self.frame1, self.frame2 = (self.frame1 + 1) % 6, (self.frame2 + 1) % 6
-        self.char1_x += self.char1_speed
-        self.char2_x += self.char2_speed
-    def handle_event(self):
-        pass
+        if self.char1_x > SCREENX:
+            self.char1_x = 0
+        else:
+            self.char1_x += self.char1_speed
+        if self.char2_x > SCREENX:
+            self.char2_x = 0
+        else:
+            self.char2_x += self.char2_speed
+        if self.logo_x >= 1000:
+            self.logo_x -= 20
+
     def draw(self):
         self.background.clip_draw(0, 0, 724, 384, self.x, self.y, SCREENX, SCREENY)
-        self.logo.clip_draw(0, 0, 290, 164, self.logo_x, 1500, 870, 492)
-        self.char1.clip_draw(self.frame1 * 93, 0, 93, 96, self.char1_x, 100, 279, 288)
-        self.char2.clip_draw(self.frame2 * 93, 0, 93, 96, self.char2_x, 200, 279, 288)
+        self.logo.clip_draw(0, 0, 290, 164, self.logo_x, 750, 650, 420)
+        self.char1.clip_draw(self.frame1 * 93, 0, 93, 96, self.char1_x, 300, 186, 192)
+        self.char2.clip_draw(self.frame2 * 93, 0, 93, 96, self.char2_x, 370, 186, 192)
