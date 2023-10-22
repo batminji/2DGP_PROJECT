758e41b5f7ac2f207c6efed2d20dffbb722093ad
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-10-22 21:06:25 +0900

    vault 리소스 추가 및 클래스 생성자에서 이미지 로드.

--- code/vault.py
+++ code/vault.py
@@ -1,9 +1,17 @@
 from pico2d import *
 
+SCREENX, SCREENY = 1915, 1015
 
 class Vault:
     def __init__(self):
-        pass
+        self.grass = load_image('resource/ground_40x40.png')
+        self.track = load_image('resource/vault_track.png')
+        self.player_walk = load_image('VAULT_PLAYER/player_walk.png')
+        self.player_run = load_image('VAULT_PLAYER/player_run.png')
+        self.player_jump = load_image('VAULT_PLAYER/player_vault_jump.png')
+        self.player_rotate = load_image('VAULT_PLAYER/player_vault_rotate.png')
+        self.player_finish = load_image('VAULT_PLAYER/player_vault_finish.png')
+        self.player_win = load_image('VAULT_PLAYER/player_win.png')
 
     def update(self):
         pass
