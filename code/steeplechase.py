fbb12facb97dc55b8d811c9f04e356152c5886f1
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-10-30 14:19:04 +0900

    steeplechase track, grass, hurdle, sky draw.

--- code/steeplechase.py
+++ code/steeplechase.py
@@ -14,10 +14,52 @@ SCREENX, SCREENY = 1915, 1015
 
 class Steeplechase:
     def __init__(self):
-        pass
+        # track
+        self.track = load_image('resource/steeplechase_track.png')
+        self.ai_track_x, self.player_track_x = 0, 0
+        self.hurdle = load_image('resource/hurdle.png')
+        self.hurdle_falldown = load_image('resource/hurdle_falldown.png')
+        self.hurdle_state = []
+        self.ai_huddle_x, self.player_huddle_x = [], []
+        for i in range(11):
+            self.hurdle_state.append(0) # 0 : common 1 : fall down
+            self.ai_huddle_x.append(1000 + i * 500)
+            self.player_huddle_x.append(1000 + i * 500)
+        # grass
+        self.bottom_grass = load_image('resource/long_grass_2257x24.png')
+        self.top_grass = load_image('resource/long_grass2_160x8.png')
+        self.bottom_grass_x, self.top_grass_x = 0, 0
+        # crowd
+        self.blue_bar = load_image('resource/blue_bar_500x25.png')
+        self.crowd = load_image('resource/crowd_500x15.png')
+        self.crowd_x = 0
+        self.blue_bar2 = load_image('resource/blue_bar2_500x6.png')
+        # sky
+        self.sky = load_image('resource/sky.png')
+        self.sky_x = 0
+
     def handle_events(self, e):
         pass
     def update(self):
         pass
     def draw(self):
-        pass
+        # sky
+        self.sky.clip_draw(self.sky_x, 0, SCREENX, 287, SCREENX / 2, 845, SCREENX, 400)
+        # crowd
+        self.blue_bar.clip_draw(0, 0, 500, 25, SCREENX // 2, 430, SCREENX, 200)
+        self.crowd.clip_draw(self.crowd_x, 0, 250, 15, SCREENX // 2, 580, SCREENX, 100)
+        self.blue_bar2.clip_draw(0, 0, 500, 6, SCREENX // 2, 655, SCREENX, 50)
+        # grass
+        self.bottom_grass.clip_draw(self.bottom_grass_x, 0, 80, 24, SCREENX // 2, 153, SCREENX, 116)
+        self.top_grass.clip_draw(self.top_grass_x, 0, 80, 8, SCREENX // 2, 310, SCREENX, 40)
+        # ai_track
+        self.track.clip_draw(self.ai_track_x, 0, SCREENX, 130, SCREENX / 2, 275, SCREENX, 130)
+        for i in range(11):
+            self.hurdle.clip_draw(0, 0, 70, 130, self.ai_huddle_x[i], 275, 70, 130)
+        # player track
+        self.track.clip_draw(self.player_track_x, 0, SCREENX, 130, SCREENX / 2, 80, SCREENX, 130)
+        for i in range(11):
+            if self.hurdle_state[i] == 0:
+                self.hurdle.clip_draw(0, 0, 70, 130, self.player_huddle_x[i], 80, 70, 130)
+            elif self.hurdle_state[i] == 1:
+                self.hurdle_falldown.clip_draw(0, 0, 127, 130, self.player_huddle_x[i], 127, 130)
