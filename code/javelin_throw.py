5bb0c262f105a261e387d5cb9b6e6fdadc9e7fed
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-11-05 23:16:53 +0900

    javelin throw player image load.

--- code/javelin_throw.py
+++ code/javelin_throw.py
@@ -4,10 +4,20 @@ SCREENX, SCREENY = 1915, 1015
 
 
 # state
+# 0 : 걷기
+# 1 : 들고 달리기
+# 2 : 던지기 1
+# 3 : 던지기 2
 
 class JavelinThrow:
     def __init__(self):
-        pass
+        # player
+        self.player_walk = load_image('THROW_PLAYER/player_walk.png')
+        self.player_run = load_image('THROW_PLAYER/player_javelin_run.png')
+        self.player_throw1 = load_image('THROW_PLAYER/player_throw_1.png')
+        self.player_throw2 = load_image('THROW_PLAYER/player_throw_2.png')
+        self.player_state = 0
+        # stick
 
     def handle_events(self, e):
         pass
