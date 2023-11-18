28f6e4ffcf4085e239ee5fa97645a7f0c42c4bd6
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-11-18 20:35:03 +0900

    long jump player image load, state, frame, x, y 초기화

--- code/long_jump.py
+++ code/long_jump.py
@@ -8,6 +8,14 @@ SCREENX, SCREENY = 1915, 1015
 
 class LongJump:
     def __init__(self):
+        # player
+        self.player_walk = load_image('JUMP_PLAYER/player_walk.png')
+        self.player_run = load_image('JUMP_PLAYER/player_run.png')
+        self.player_longjump = load_image('JUMP_PLAYER/player_longjump.png')
+        self.player_win = load_image('JUMP_PLAYER/player_win.png')
+        self.player_state = 'WALK'
+        self.player_frame, self.player_x, self.player_y = 0, 0, 0
+
         pass
 
     def handle_events(self, e):
