4800f84b8762340b114206da4ed00c05a7b5f7b3
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-11-23 23:52:00 +0900

    game over python file 생성.

---
+++ code/gameover.py
@@ -0,0 +1,20 @@
+from pico2d import *
+import game_world
+
+
+SCREENX, SCREENY = 1915, 1015
+
+
+class GameOver:
+    def __init__(self):
+        pass
+
+    def handle_events(self, e):
+        pass
+
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
